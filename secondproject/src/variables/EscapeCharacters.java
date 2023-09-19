835fde96ef8ebf1fb48ef9d838a4e23dd7e0e2fc
Terence Agaromba <teroindian@gmail.com>
2023-09-19 14:13:46 -0400

    comment added

--- secondproject/src/variables/EscapeCharacters.java
+++ secondproject/src/variables/EscapeCharacters.java
@@ -5,12 +5,14 @@ public class EscapeCharacters {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//The main ones to know are \t, \n and  \:
 		//this is an example of tab escape character
 		System.out.println("This is a tab character ->\t<-");
 		
 		//basic formatting with tabs
 		System.out.println("1\tone\t\tCol3");
 		System.out.println("10\tTen\t\tCol3");
+		//this one is long so it need one tab character
 		System.out.println("100\tOne Hundred\tCol3");
 		
 		//this an example of a new line character \n
