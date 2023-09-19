9bdeb7ffaed75dc01137e26a9d005923f2e4db2d
Terence Agaromba <indiantero@gmail.com>
2023-09-19 10:24:18 -0400

    First project commited

---
+++ FirstProject/src/firstclass/FisrtClass.java
@@ -0,0 +1,18 @@
+package firstclass;
+
+public class FisrtClass {
+
+	
+public static void main(String[] args) {
+	
+	//This is a single line comment
+	
+	/* This is a 
+	 * multiple comment
+	 */
+	
+System.out.println("Welcome to Java!");
+}
+
+
+}
