68a4b94d41ab78ded1db47bc7ef6ed2c78268c6b
Terence Agaromba <teroindian@gmail.com>
2023-09-19 14:05:36 -0400

    escape characters lesson

---
+++ secondproject/src/variables/EscapeCharacters.java
@@ -0,0 +1,25 @@
+package variables;
+
+public class EscapeCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//this is an example of tab escape character
+		System.out.println("This is a tab character ->\t<-");
+		
+		//basic formatting with tabs
+		System.out.println("1\tone\t\tCol3");
+		System.out.println("10\tTen\t\tCol3");
+		System.out.println("100\tOne Hundred\tCol3");
+		
+		//this an example of a new line character \n
+		System.out.println("This is line 1 \nThis is line 2");
+		System.out.println("This is line 3");
+		
+		//What happens if i want to print just a \
+		System.out.println("To get a slash to print we need 2 of them \\");
+
+	}
+
+}
