070bfb5f6879869ed12fc8fb5bf0d4d0bfa40307
Terence Agaromba <teroindian@gmail.com>
2023-10-16 11:25:01 -0400

    commenting exception file

--- secondproject/src/practiseassignmentslab/InvalidInputException.java
+++ secondproject/src/practiseassignmentslab/InvalidInputException.java
@@ -1,7 +1,8 @@
-/*package practiseassignmentslab;
+package practiseassignmentslab;
 
-
-	class InvalidInputException extends Exception { 
+/*
+  public class InvalidInputException extends Exception { 
+	  
 		public InvalidInputException(String message) { 
 			super(message);
 		}
