c88a068e392a4032ff0616af42891a1790218c43
Terence Agaromba <teroindian@gmail.com>
2023-10-31 09:19:53 -0400

    junit testing Glab

--- junit-examples/src/test/java/perscholas/testing/CalculatorTest.java
+++ junit-examples/src/test/java/perscholas/testing/CalculatorTest.java
@@ -2,9 +2,45 @@ package perscholas.testing;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculatorTest {
 
+	
+	
+	@ParameterizedTest
+    @CsvSource({
+            "5, 5, 10",
+            "10, 10, 20",
+            "5.5, 5.5, 11.0"
+    })
+	public void additionTest(double a, double b, double answer) { 
+		
+		Calculator c = new Calculator ();
+		
+		double actual = c.add(a,b);
+		Assertions.assertEquals(answer,actual);
+		
+	}
+	
+	@ParameterizedTest
+	@CsvSource({ 
+		 "5, 5, 10.5",
+         "10, 10, 20.2",
+         "5.5, 5.5, 11.6"
+         
+	})
+	public void additionErrorTest(double a,double b,double answer) {
+		Calculator c = new Calculator();
+		
+		double actual = c.add(a,b);
+		
+	
+		Assertions.assertNotEquals(answer,actual);
+		
+	}
+	
 	
 	@Test
 	public void additionTest() {
