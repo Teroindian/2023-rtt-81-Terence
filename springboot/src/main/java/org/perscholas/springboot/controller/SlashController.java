0e387bf45c7615ca6f9aa2264c3cd011de4ef613
TerenceAgaromba <indiantero@gmail.com>
2023-11-27 09:24:22 -0500

    springboot class

--- springboot/src/main/java/org/perscholas/springboot/controller/SlashController.java
+++ springboot/src/main/java/org/perscholas/springboot/controller/SlashController.java
@@ -3,14 +3,28 @@ package org.perscholas.springboot.controller;
 
 import org.perscholas.springboot.database.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SlashController {
 
-    public void index ( ) {
+   /* public void index ( ) {
         User user = new User();
 
         user.setId(1);
         user.setUsername( "eric@webworkbench.net");
+    }*/
+
+    @GetMapping("/index")
+    public ModelAndView index() {
+        ModelAndView response = new ModelAndView("index");
+        return response;
     }
+    @GetMapping("/about")
+    public ModelAndView about() {
+        ModelAndView response = new ModelAndView("about");
+        return response;
+    }
+
 }
