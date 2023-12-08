16e93032a835e86f8ac75953f4113e4004723945
TerenceAgaromba <indiantero@gmail.com>
2023-12-08 13:36:38 -0500

    fixed  imports

--- springboot/src/main/java/org/perscholas/springboot/controller/ErrorController.java
+++ springboot/src/main/java/org/perscholas/springboot/controller/ErrorController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.nio.file.AccessDeniedException;
+import org.springframework.security.access.AccessDeniedException;
+//import java.nio.file.AccessDeniedException;
 
 @Slf4j
 @Controller
