00b49555687c121fb75a8ebb782e70c2c37b5587
TerenceAgaromba <indiantero@gmail.com>
2023-12-13 13:27:35 -0500

    file upload

--- springboot/src/main/java/org/perscholas/springboot/controller/CustomerController.java
+++ springboot/src/main/java/org/perscholas/springboot/controller/CustomerController.java
@@ -4,6 +4,7 @@ package org.perscholas.springboot.controller;
 import io.micrometer.common.util.StringUtils;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.perscholas.springboot.database.dao.CustomerDAO;
 import org.perscholas.springboot.database.entity.Customer;
 import org.perscholas.springboot.database.entity.User;
@@ -17,9 +18,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 @Slf4j
@@ -240,6 +246,41 @@ public class CustomerController {
 
         return response;
     }*/
+
+    @GetMapping("/customer/fileupload")
+    public ModelAndView fileUpload() {
+        ModelAndView response = new ModelAndView("customer/fileupload");
+
+        log.info(" In fileupload with no Args");
+        return response;
+    }
+
+
+
+    @PostMapping("/customer/fileUploadSubmit")
+    public ModelAndView fileUploadSubmit(@RequestParam("file") MultipartFile file) {
+        ModelAndView response = new ModelAndView("customer/fileupload");
+
+        log.info("Filename = " + file.getOriginalFilename());
+        log.info("Size     = " + file.getSize());
+        log.info("Type     = " + file.getContentType());
+
+
+        // Get the file and save it somewhere
+        File f = new File("./src/main/webapp/pub/images/" + file.getOriginalFilename());
+        try (OutputStream outputStream = new FileOutputStream(f.getAbsolutePath())) {
+            IOUtils.copy(file.getInputStream(), outputStream);
+        } catch (Exception e) {
+
+
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+
+
 
 
 
---
+++ springboot/src/main/webapp/WEB-INF/jsp/customer/fileupload.jsp
@@ -0,0 +1,25 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<jsp:include page="../include/header.jsp"/>
+
+<section>
+    <div class="bg-light2 pt-5 pb-5">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h1 class="m-0">File upload</h1>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5 pb-5">
+    <div class="container">
+        <form method="POST" action="/customer/fileUploadSubmit" enctype="multipart/form-data">
+            <input type="file" name="file" /><br/><br/>
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
+</section>
+
+
+<jsp:include page="../include/footer.jsp"/>
